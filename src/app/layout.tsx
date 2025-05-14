51deecb8b90bbdf160f7927a9f7a4c9da0762f27
Mwlama <167961776+Mwlama@users.noreply.github.com>
2025-05-14 14:48:36 +0400

    bot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,11 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className="scrollbar">
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="I2GPG9awCcjxSDHZP4wCd";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`,
+            }}
+        />
             <body
                 className={cn(
                     "min-h-screen bg-background text-foreground antialiased !font-default overflow-x-hidden",
@@ -25,6 +30,7 @@ export default function RootLayout({
                     <Providers>
                         <Toaster richColors theme="dark" position="top-right" />
                         {children}
+                        
                     </Providers>
                 </Suspense>
             </body>
